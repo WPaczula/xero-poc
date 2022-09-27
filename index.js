6876cf78f988996875920a609d4f7cc6a43ac8bf
@wojtek-yordex <wojtek@yordex.com>
2022-09-27 15:54:17 +0200

    Remove contacts fetching

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const xero = new XeroClient({
   clientId: '7B0DDDBFA1C04610AD6E4EC9AF452CA7',
   clientSecret: 'MCeAoRQuLdSP7GvsoxCAQmMUWewBg1sZJqRP6ERFkf1uYJeN',
   redirectUris: [`https://xero-poc.herokuapp.com/callback`],
-  scopes: 'openid profile email accounting.transactions offline_access'.split(" "),
+  scopes: 'openid profile email offline_access'.split(" "),
 });
 
 const app = express();
@@ -30,26 +30,6 @@ const getOrganisation = async (tenantId) => {
   return organisation
 }
 
-const getContactsCount = async (tenantId) => {
-  const contactsCount = 0;
-  let page = 1;
-  let shouldContinue = true;
-
-  while (shouldContinue) {
-    const contactsPerPage = (await xero.accountingApi.getContacts(tenantId, undefined, undefined, undefined, undefined, page)).body.contacts;
-    console.log('contacts per page', page, contactsPerPage)
-
-    if (contactsPerPage && contactsPerPage.length) {
-      page += 1;
-      contactsCount += contactsPerPage.length
-    } else {
-      shouldContinue = false
-    }
-  }
-
-  return contactsCount
-}
-
 app.get('/callback', async (req, res) => {
   const { id_token } = await xero.apiCallback(req.url);
   const { given_name, family_name, email } = jwtDecode(id_token);
@@ -64,9 +44,7 @@ app.get('/callback', async (req, res) => {
     const organisation = await getOrganisation(tenantId);
     console.log('organisation: ', organisation)
     console.log('======================================')
-    console.log('getting contact counts')
-    const contactsCount = await getContactsCount(tenantId);
-    console.log('count: ', contactsCount)
+    console.log('redirect')
     res.redirect(`https://yordex.webflow.io/book-a-demo?lastName=${encodeURIComponent(family_name)}&firstName=${encodeURIComponent(given_name)}&email=${encodeURIComponent(email)}&company=${encodeURIComponent(organisation.name)}`)
   } catch (error) {
     console.error(error)
