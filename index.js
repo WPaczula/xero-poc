f4f18e4f4116f1092be8b16e832d42f55b1310a7
@wojtek-yordex <wojtek@yordex.com>
2022-09-27 12:38:25 +0200

    fix redirecrUris

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 const xero = new XeroClient({
   clientId: '7B0DDDBFA1C04610AD6E4EC9AF452CA7',
   clientSecret: 'MCeAoRQuLdSP7GvsoxCAQmMUWewBg1sZJqRP6ERFkf1uYJeN',
-  redirectUris: [`http://https://xero-poc.herokuapp.com/callback`],
+  redirectUris: [`https://xero-poc.herokuapp.com/callback`],
   scopes: 'openid profile email accounting.transactions offline_access'.split(" "),
 });
 
