212d9b618d59373036ffbf370cdda90ce6b830c9
@wojtek-yordex <wojtek@yordex.com>
2022-09-27 12:31:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const { XeroClient } = require('xero-node');
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+
+const xero = new XeroClient({
+  clientId: '7B0DDDBFA1C04610AD6E4EC9AF452CA7',
+  clientSecret: 'MCeAoRQuLdSP7GvsoxCAQmMUWewBg1sZJqRP6ERFkf1uYJeN',
+  redirectUris: [`http://localhost:${port}/callback`],
+  scopes: 'openid profile email accounting.transactions offline_access'.split(" "),
+});
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get('/callback', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/callback.html'))
+})
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
